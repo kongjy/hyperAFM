ac2653c3412b77d8947d02886859f42606beb8c3
Jessica <kongjy@uw.edu>
2017-06-10 18:24:16 -0700

    moved all other functions except for AllRelativeIntensities to their own file.

--- Jessica/AllRelativePeakIntensities.py
+++ Jessica/AllRelativePeakIntensities.py
@@ -9,67 +9,6 @@ Created on Mon May  1 09:00:31 2017
 import numpy as np
 import FindPeaks
 
-
-def TotIntensity(spectrum, thres, min_dist):
-    """Takes a spectrum and returns summed intensity at each data point.
-    
-    INPUT: spectrum: list of intensities
-       
-        thres: minimum relative intensity required to be considered a peak.
-        Only peaks with relative amplitude higher than threshold will 
-        be counted.
-        
-        min_dist: minimum index spacing between two peaks. Setting this to
-        1 means that the minimum distance between two peaks is the resolution
-        of the IR spectrum, which is specified during the experiment. 
-    
-    RETURNS: number, reflecting sum of intensities at each wavenumber 
-                in spectrum.
-    """
-    peakindices, allpeaks = FindPeaks.FindPeaks(spectrum, thres, min_dist)
-    
-    totintensity = sum(allpeaks.values())
-    return totintensity
-
-
-def RelIntensities(spectrum, peak1, peak2): 
-    """Calculates the relative peak intensities of two peaks in a spectrum.
-    
-    INPUTS: spectrum: list of intensities
-            peak1: index of peak 1
-            peak2: index of peak 2
-    RETURNS: relative intensity of peak1/peak2
-    
-    Takes a spectrum and the position of two peaks
-    """
-    #consider: peak indices or peak positions? 
-    #calc relative peak intensities
-    peak12 = spectrum[peak1]/spectrum[peak2]
-    return peak12
-
-
-def FindSamePeaks(indices, peaksdict, otherspectrum):
-    """This function takes the indices of peak positions and finds the 
-    intensity of those same peaks in a different spectrum. 
-    
-    INPUTS: indices: list of indices from FindPeaks
-            peaksdict: dictionary of peak positions and intensities from 
-                        FindPeaks
-            otherspectrum: function will look in this spectrum for the same
-                        peaks 
-
-    RETURNS: peaksdict with the intensity of the peak in other 
-             spectrum appended. 
-    """
- 
-    for index in indices:
-        key=index
-        otherpeakintensity=otherspectrum[index]
-        peaksdict[key].append(otherpeakintensity)
-        
-    return peaksdict
-
-
 def AllRelativeIntensities(spectrum, thres, min_dist):
     """Finds the relative intensities of every peak in the spectrum. 
     
